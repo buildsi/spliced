51f0e5d967765ef2b6d6507980fa4c1ed6083ba1
vsoch <vsoch@users.noreply.github.com>
2021-12-07 13:37:46 -0700

    have action define baseurl and repo
    Signed-off-by: vsoch <vsoch@users.noreply.github.com>

--- action/ui/scripts/generate.sh
+++ action/ui/scripts/generate.sh
@@ -10,10 +10,24 @@ if [ ! -d "${INPUT_ARTIFACTS}" ]; then
     exit
 fi
 
+# Repository name
+repository_name=$(basename ${PWD})
+printf "Repository name is ${repository_name}\n"
+
+if [ -z "${INPUT_REPO}" ]; then
+    INPUT_REPO=${INPUT_REPOSITORY}
+fi
+
 # If we don't have the docs directory yet, copy it there
 if [ ! -d "${INPUT_DOCS}" ]; then
     printf "Output directory ${INPUT_DOCS} does not exist yet, copying...\n"
     cp -R ${ACTION_PATH}/docs ${INPUT_DOCS}
+
+   # Ensure repository and baseurl at top of _config
+   sed -i "1irepository: ${INPUT_REPO}" ${INPUT_DOCS}/_config.yml
+   sed -i "1ibaseurl: /${repository_name}" ${INPUT_DOCS}/_config.yml
+   cat ${INPUT_DOCS}/_config.yml
+
 fi
 
 if [ -z ${INPUT_EXPERIMENT+x} ]; then 
